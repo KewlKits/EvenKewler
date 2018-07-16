4eedf695d86fb39f2199050dc9b96f64190c4427
Natalie Ghidali <nghidali@fb.com>
2018-07-16 14:47:40 -0700

    Added Basic login functionality using OAuth

---
+++ SpotifyTest/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  SpotifyTest
+//
+//  Created by Natalie Ghidali on 7/16/18.
+//  Copyright © 2018 Natalie Ghidali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SafariServices/SafariServices.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, SPTAudioStreamingDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
